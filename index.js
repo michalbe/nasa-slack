ca902af7a4cfb0577f252ee08aafc860748d373d
Michał Budzyński <michal@virtualdesign.pl>
2018-12-08 21:40:00 +0100

    :open_file_folder: working example

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 const Slack = require('slack-node');
+const Request = require('request-promise-native');
+
 const apiToken = process.argv[2];
 
 slack = new Slack(apiToken);
 
-slack.api("users.list", function (err, response) {
-    console.log(response);
-});
+Request.get('https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo')
+.then((result) => {
+    try {
+        result = JSON.parse(result);
+        console.log(result);
 
-slack.api('chat.postMessage', {
-    text: 'test',
-    username: 'OAK & Nasa photo of the day',
-    icon_emoji: ":rocket:",
-    channel: '#t2'
-}, function (err, response) {
-    console.log(response);
+        slack.api('chat.postMessage', {
+            text: '',
+            attachments: JSON.stringify([
+                {
+                    "fallback": "Photo of the day",
+                    "color": "#2eb886",
+                    "author_name": "Oak & NASA",
+                    "pretext": "Oak & Nasa Photo of the day",
+                    "title": result.title,
+                    "title_link": result.hdurl,
+                    "text": result.explanation,
+                    "image_url": result.hdurl,
+                    "thumb_url": result.url,
+                    "footer": "by Osiedlowa Agencja Kosmiczna",
+                    "footer_icon": ":rocket:",
+                    "ts": 123456789
+                }
+            ]),
+            username: 'OAK & Nasa photo of the day',
+            icon_emoji: ":rocket:",
+            channel: '#t2'
+        }, function (err, response) {
+            console.log(response);
+        });
+
+    } catch(e) {
+        console.error(e);
+        process.exit(1);
+    }
+
 });
