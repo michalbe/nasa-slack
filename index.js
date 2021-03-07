70be343543adff1a01878abe392e7a58685ff1e3
Michał Budzyński <michal@virtualdesign.pl>
2021-03-07 19:24:10 +0100

    mattermost version

--- index.js
+++ index.js
@@ -1,51 +1,45 @@
-const Slack = require('slack-node');
 const Request = require('request-promise-native');
 const Cheerio = require('cheerio');
 const apiToken = process.argv[2];
+const Mattermost = require('node-mattermost');
 
-slack = new Slack(apiToken);
+const mattermost = new Mattermost(
+	`https://mattermost.jelocartel.com/hooks/${apiToken}`,
+	{}
+);
 
-Request.get('https://apod.nasa.gov/apod/astropix.html')
-.then((result) => {
+const date = new Date();
+const year = (date.getYear() - 100).toString();
+const month = (date.getMonth() + 1).toString().padStart(2, '0');
+const day = date.getDate().toString().padStart(2, '0');
 
-    try {
-        // result = JSON.parse(result);
-        const $ = Cheerio.load(result);
-        // console.log(result);
+Request.get('https://apod.nasa.gov/apod/astropix.html').then((result) => {
+	try {
+		const $ = Cheerio.load(result);
+		const url = 'https://apod.nasa.gov/apod/' + $('img').eq(0).attr('src');
+		const title = $('center > b').eq(0).text().trim();
+		const explanation = $('p')
+			.eq(2)
+			.text()
+            .replace('Explanation: ', '').trim();
+		mattermost
+			.send({
+				text: `[${title}](https://apod.nasa.gov/apod/ap${year + month + day}.html)
+---
+${explanation}
 
-        const url = 'https://apod.nasa.gov/apod/' + $('img').eq(0).attr('src');
-        console.log(url);
-        const title = $('center > b').eq(0).text().trim();
-        const explanation = $('p').eq(2).text().replace(/\r?\n|\r/g, ' ');
-        console.log(explanation);
-        slack.api('chat.postMessage', {
-            text: '',
-            attachments: JSON.stringify([
-                {
-                    "fallback": "Photo of the day",
-                    "color": "#2eb886",
-                    "author_name": "Oak & NASA",
-                    "pretext": "Oak & Nasa Photo of the day",
-                    "title": title,
-                    "title_link": url,
-                    "text": explanation,
-                    "image_url": url,
-                    "thumb_url": url,
-                    "footer": "by Osiedlowa Agencja Kosmiczna",
-                    "footer_icon": ":rocket:",
-                    "ts": 123456789
-                }
-            ]),
-            username: 'OAK & Nasa photo of the day',
-            icon_emoji: ":rocket:",
-            channel: '#t2'
-        }, function (err, response) {
-            console.log(response);
-        });
-
-    } catch(e) {
-        console.error(e);
-        process.exit(1);
-    }
-
+![${url}](${url})
+`,
+				channel: '#t2'
+			})
+			.then((e) => {
+				console.log(e);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	} catch (e) {
+		console.error(e);
+		process.exit(1);
+	}
 });
